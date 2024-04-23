3f3498c4d59cfc924c9b1f632188f96732c4c965
Hassan <muhammadhassanchannel786@gmail.com>
2024-04-23 17:46:06 +0500

    feat: clerk auth

---
+++ app/(main)/(clerk)/register/[[...register]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return <SignUp path="/register" />
+}
+
+export default SignUpPage
---
+++ app/(main)/test/page.tsx
@@ -0,0 +1,12 @@
+import { SignOutButton } from '@clerk/nextjs'
+import React from 'react'
+
+const TestPage = () => {
+  return (
+    <div>TestPage
+      <SignOutButton />
+    </div>
+  )
+}
+
+export default TestPage
