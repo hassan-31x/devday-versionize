26dc05ecfc8b5a6591456ff8c9fa249b65bd10d2
Hassan <muhammadhassanchannel786@gmail.com>
2024-04-23 19:22:54 +0500

    feat: public routes protected if user logged in

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,30 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
  
 export default authMiddleware({
     publicRoutes: ["/"],
+    afterAuth(auth, req) {
+      if (auth.userId && auth.isPublicRoute) {
+        let path = '/select-org'
+
+        if (auth.orgId) {
+          path = `/organization/${auth.orgId}`
+        }
+
+        const orgSelection = new URL(path, req.url)
+        return NextResponse.redirect(orgSelection)
+      }
+
+      if (!auth.userId && !auth.isPublicRoute) {
+        return RedirectToSignIn({ signInFallbackRedirectUrl: req.url })
+      }
+
+      if (auth.userId && !auth.orgId && req.nextUrl.pathname !== '/select-org') {
+        const orgSelection = new URL('/select-org', req.url)
+        return NextResponse.redirect(orgSelection)
+      }
+    }
 });
  
 export const config = {
