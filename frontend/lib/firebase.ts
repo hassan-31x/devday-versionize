e1e424a016eaef2cbb42c8ff63eb59e794ca22fb
Mujeeb ur rehman <mujeebfx160@gmail.com>
2024-04-24 23:53:11 +0500

    Add : firebase & package

---
+++ frontend/lib/firebase.ts
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebaseConfig";
+import { getDatabase } from 'firebase/database';
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseDatabase = getDatabase(firebaseApp); // Initialize the database
+
+export { firebaseApp as app, firebaseDatabase as database };
---
+++ frontend/lib/firebaseConfig.ts
@@ -0,0 +1,16 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3TFI9BJSg6-fHbM_E_ZLLFdqWv3Agijw",
+  authDomain: "gitmanagment.firebaseapp.com",
+  databaseURL: "https://gitmanagment-default-rtdb.firebaseio.com",
+  projectId: "gitmanagment",
+  storageBucket: "gitmanagment.appspot.com",
+  messagingSenderId: "802026035901",
+  appId: "1:802026035901:web:5db68ef0b8f3000ae175d4",
+  measurementId: "G-NNTCM9GE0X"
+};
