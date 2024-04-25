57236729d4dcf04989eae2780ada3b4af7a5325e
Hassan <muhammadhassanchannel786@gmail.com>
2024-04-25 06:32:51 +0500

    feat: add file

--- app/(protected)/(main)/organization/[organizationId]/_components/file.tsx
+++ app/(protected)/(main)/organization/[organizationId]/_components/file.tsx
@@ -1,13 +1,16 @@
 import { storage } from "@/lib/firebaseConfig";
 import { getDownloadURL, ref } from "firebase/storage";
 import { File, FileImage, Trash2 } from "lucide-react";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   fileName: string;
   fileType: "doc" | "image";
   path: string;
-  handleDelete: (path: string) => void;
+  handleDelete: (path: string, commitMessage: string) => void;
 };
 
 const SingleFile = ({ fileName, fileType, path, handleDelete }: Props) => {
@@ -15,7 +18,9 @@ const SingleFile = ({ fileName, fileType, path, handleDelete }: Props) => {
     doc: <File className="h-4 w-4" />,
     image: <FileImage className="h-4 w-4" />,
   };
+  const [commitMessage, setCommitMessage] = useState("");
   const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const fetchDownloadUrl = async () => {
@@ -26,6 +31,11 @@ const SingleFile = ({ fileName, fileType, path, handleDelete }: Props) => {
 
     fetchDownloadUrl();
   }, [fileName]);
+
+  const delete2 = () => {
+    handleDelete(path, commitMessage)
+    setIsOpen(false)
+  }
 
   return (
     <div className="w-full hover:bg-slate-50 h-8 border px-2 border-gray-400 flex justify-between items-center">
@@ -40,8 +50,22 @@ const SingleFile = ({ fileName, fileType, path, handleDelete }: Props) => {
         )}
       </div>
 
+      <Dialog open={isOpen}>
+        <DialogTrigger>
+          <Trash2 className="h-4 w-4 cursor-pointer text-red-600 hover:text-red-500" />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="leading-6">Are you absolutely sure you want to delete '{fileName}'?</DialogTitle>
+            <DialogDescription className="pt-1">This action cannot be undone. This will delete your file. However, you can recover it later from commit history.</DialogDescription>
+          </DialogHeader>
 
-      <Trash2 className="h-4 w-4 cursor-pointer text-red-600 hover:text-red-500" onClick={() => handleDelete(path)} />
+          <div className="flex gap-3 items-center">
+          <Input value={commitMessage} onChange={e => setCommitMessage(e.target.value)} placeholder="commit message" />
+          <Button onClick={delete2} variant='destructive'>Delete</Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
--- app/(protected)/(main)/organization/[organizationId]/page.tsx
+++ app/(protected)/(main)/organization/[organizationId]/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { db } from "@/lib/firebaseConfig";
+import { ChangeEvent, useEffect, useState } from "react";
+import { db, storage } from "@/lib/firebaseConfig";
 import SingleFile from "./_components/file";
 import { addDoc, collection, getDocs, query, serverTimestamp, where } from "firebase/firestore";
 import { toast } from "sonner";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
+import { FileCheck2, FilePlus2, Upload } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Image from "next/image";
 
 type Props = {
   params: {
@@ -13,7 +21,14 @@ type Props = {
 };
 
 const OrganizationIdPage: React.FC<Props> = ({ params: { organizationId } }: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
   const [files, setFiles] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [url, setUrl] = useState({ pathRef: "", url: "" } as any);
+  const [commitMessage, setCommitMessage] = useState("");
+
+  const router = useRouter();
 
   const getFileType = (fileName: string) => {
     const fileExtension = fileName.split(".").pop();
@@ -27,6 +42,10 @@ const OrganizationIdPage: React.FC<Props> = ({ params: { organizationId } }: Pro
   };
 
   useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
     const fetchCommits = async () => {
       try {
         const projectCollection = collection(db, "projects");
@@ -38,66 +57,149 @@ const OrganizationIdPage: React.FC<Props> = ({ params: { organizationId } }: Pro
           const commitsQuery = query(commitsCollectionRef);
           const commitsSnapshot = await getDocs(commitsQuery);
 
-          //@ts-ignore
-          commitsSnapshot.forEach(async (commitDoc, idx) => {
-            if (idx > 0) return;
+          const { files } = commitsSnapshot.docs[0].data();
 
-            const { files } = commitDoc.data();
-
-            setFiles([
-              ...files.map((file: any, idx: number) => {
-                return {
-                  id: idx,
-                  path: file,
-                  name: getFileName(file),
-                  type: getFileType(file),
-                };
-              }),
-            ]);
-          });
+          setFiles([
+            ...files.map((file: any, idx: number) => {
+              return {
+                id: idx,
+                path: file,
+                name: getFileName(file),
+                type: getFileType(file),
+              };
+            }),
+          ]);
         });
       } catch (error) {
         console.error("Error fetching commits:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchCommits();
   }, [organizationId]);
 
-  useEffect(() => {
-    console.log(files);
-  }, [files]);
+  if (!isMounted) return null;
 
-  const handleDelete = async (path: string) => {
+  const handleDelete = async (path: string, commitMessage: string) => {
     try {
+      const newFiles = files.filter((file) => file.path !== path).map((file) => file.path);
 
-      const newFiles = files.filter(file => file.path !== path).map(file => file.path);
-      
       const projectCollection = collection(db, "projects");
       const q = query(projectCollection, where("projectId", "==", organizationId));
       const projectSnapshot = await getDocs(q);
       const projectDoc = projectSnapshot.docs[0];
-      
+
       const commitsCollectionRef = collection(projectDoc.ref, "commits");
       addDoc(commitsCollectionRef, {
+        message: commitMessage,
         createdAt: serverTimestamp(),
-        files: newFiles
-      })
+        files: newFiles,
+      });
 
       toast.success("File deleted successfully");
-      
+      router.refresh();
     } catch (error) {
       console.error("Error deleting file:", error);
       toast.error("Error deleting file");
     }
-  }
+  };
+
+  const handleAdd = async (commitMessage: string) => {
+    if (url.pathRef === "" || commitMessage === '') return;
+    try {
+      const newFiles = [...files.map((file: any) => file.path), `/${url.pathRef}`];
+
+      const projectCollection = collection(db, "projects");
+      const q = query(projectCollection, where("projectId", "==", organizationId));
+      const projectSnapshot = await getDocs(q);
+      const projectDoc = projectSnapshot.docs[0];
+
+      const commitsCollectionRef = collection(projectDoc.ref, "commits");
+      addDoc(commitsCollectionRef, {
+        message: commitMessage,
+        createdAt: serverTimestamp(),
+        files: newFiles,
+      });
+
+      toast.success("File Added successfully");
+      router.refresh();
+    } catch (error) {
+      console.error("Error adding file:", error);
+      toast.error("Error adding file");
+    }
+  };
+
+  const uploadImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const imageUpload = e.target.files[0];
+
+      const uniqueName = `${organizationId}/${imageUpload.name}`;
+
+      const imageRef = ref(storage, uniqueName);
+      await uploadBytes(imageRef, imageUpload);
+
+      const pathRef = ref(storage, uniqueName);
+      const url = await getDownloadURL(pathRef);
+      console.log("🚀 ~ uploadImage ~ url:", pathRef, url)
+      setUrl({ pathRef, url });
+    }
+  };
 
   return (
     <div className="w-full">
-      {files.length > 0 ? (
+      <Dialog open={isOpen}>
+        <DialogTrigger className="w-full">
+          <div className="w-full mt-2 mb-10 flex justify-end">
+            <Button variant="ghost" size="icon" className="rounded-full p-2" onClick={() => setIsOpen(true)}>
+              <FilePlus2 className="h-6 w-6 text-blue-500 cursor-pointer hover:text-blue-600" />
+            </Button>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="leading-6">Select file</DialogTitle>
+            <DialogDescription className="pt-1">Select any file to upload and add a commit message</DialogDescription>
+          </DialogHeader>
+
+          <div className="">
+            {url?.url ? (
+              <div className="h-48 w-full bg-slate-100 rounded-xl flex items-center justify-center">
+                <FileCheck2 className="h-7 w-7 text-green-500" />
+              </div>
+            ) : (
+              <label className="cursor-pointer">
+                <input className="hidden" type="file" onChange={uploadImage} />
+                <div className="h-48 w-full bg-slate-100 rounded-xl flex items-center justify-center">
+                  <Upload className="h-7 w-7" />
+                </div>
+              </label>
+            )}
+            <div className="flex gap-3 items-center mt-5">
+              <Input value={commitMessage} onChange={(e) => setCommitMessage(e.target.value)} placeholder="commit message" />
+              <Button onClick={handleAdd}>Add</Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+      {loading ? (
+        <>
+          <div className="flex flex-col gap-1">
+            {Array.from({ length: 6 }).map((_, idx) => (
+              <Skeleton key={idx} className="h-8 w-full" />
+            ))}
+          </div>
+          <div className="flex flex-col gap-1 mt-5">
+            {Array.from({ length: 2 }).map((_, idx) => (
+              <Skeleton key={idx} className="h-8 w-full" />
+            ))}
+          </div>
+        </>
+      ) : files.length > 0 ? (
         files.map((file) => <SingleFile key={file.id} fileName={file.name} fileType={file.type} path={file.path} handleDelete={handleDelete} />)
       ) : (
-        <div className="w-full hover:bg-slate-50 h-8 border px-2 border-gray-400 flex  justify-between items-center"></div>
+        <h3 className="font-medium text-lg text-center">No files to show</h3>
       )}
     </div>
   );
